68c2852e466e4ef9bfa46cee466bba5fbe9e5779
sabilah117 <sabilah117@gmail.com>
2021-09-24 16:17:58 +0700

    Upload awal project

---
+++ contoh1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Laila Salsa Wulan Sabilah
+ Saya tinggal di daerah Bekasi
+ olahraga yang saya sukai adalah
+ badminton";
+    }
+}
